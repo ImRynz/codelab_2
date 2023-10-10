2266e761e2c5d76fc087a0cda918ca786610bf30
ImRynz <127405171+ImRynz@users.noreply.github.com>
2023-10-10 15:42:15 +0700

    Update main.js

--- 1/main.js
+++ 1/main.js
@@ -2,13 +2,8 @@ function jumlahkan(){
     var bilangan1 = parseFloat(document.getElementById("bilangan1").value);
     var bilangan2 = parseFloat(document.getElementById("bilangan2").value);
 
-    if(isNaN(bilangan1) || isNaN(bilangan2)){
-        alert("masukkan bilangan yang valid.");
-    }
-    else{
-        var hasil = bilangan1 + bilangan2;
-        alert("hasil penjumlahan: " + hasil);
-    }
+    var hasil = bilangan1 + bilangan2;
+    alert("hasil penjumlahan: " + hasil);
 }
 
 function resetForm(){
@@ -17,4 +12,4 @@ function resetForm(){
 }
 
 document.getElementById("jumlahkan").addEventListener("click", jumlahkan)
-document.getElementById("ulang").addEventListener("click", jumlahkan)
+document.getElementById("ulang").addEventListener("click", resetForm)
